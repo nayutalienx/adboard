e98a655be45756d12eea0277077fac6b3faa5e8b
danminoranskiy@gmail.com <danminoranskiy@gmail.com>
2019-09-29 19:56:59 +0000

    Added file Advert.cs

---
+++ PresentationLayer/AdboardConsoleUI/Advert.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace at
+{
+    public class Advert
+    {
+        public string Name;
+        public string Description;
+        public long Id;
+        public DateTime Created;
+        public Author author;
+    }
+}
