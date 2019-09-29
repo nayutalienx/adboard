7911ef99814877a211141a8fefda62dd589bf0f9
danminoranskiy@gmail.com <danminoranskiy@gmail.com>
2019-09-29 19:53:08 +0000

    Added file Author.cs

---
+++ PresentationLayer/AdboardConsoleUI/Author.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace at
+{
+    public class Author
+    {
+        public string phonenumber;
+        public long AuthorId;
+        public string login;
+        public string password;
+        public bool IsAuthorized;
+    }
+}
