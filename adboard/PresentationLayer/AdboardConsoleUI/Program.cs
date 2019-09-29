65119984758746b607fe887b4975713ff2ded22d
nayutalienx <nayutalienx@gmail.com>
2019-09-29 18:46:25 +0300

    body

---
+++ adboard/PresentationLayer/AdboardConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdboardConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
